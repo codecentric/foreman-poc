e04084d52b4668cfd073fbca8ad5dc7c66abd402
Jan-Frederic Markert <Jan-Frederic.Markert@codecentric.de>
2013-12-09 12:32:10 +0100

    DDNS Konfiguration

--- manifests/server.pp
+++ manifests/server.pp
@@ -49,20 +49,24 @@ package { "foreman-installer":
 	require	=> Exec['apt-update'],
 }
 
-# seperate bind installation necessary for configuration
+# seperate installations necessary for proper configuration
 package { "bind9":
 	ensure	=> "installed",
 	require	=> Exec['apt-update'],
 }
-
-# DHCP configuration
-file { "/etc/dhcp/dhcpd.conf":
-	ensure	=> present,
-	source	=> "/vagrant/files/DHCP/dhcpd.conf",
-	owner	=> root,
-	group	=> root,
-	mode	=> 644,
+package { "isc-dhcp-server":
+	ensure	=> "installed",
+	require	=> Exec['apt-update'],
 }
+
+## DHCP configuration
+#file { "/etc/dhcp/dhcpd.conf":
+#	ensure	=> present,
+#	source	=> "/vagrant/files/DHCP/dhcpd.conf",
+#	owner	=> root,
+#	group	=> root,
+#	mode	=> 644,
+#}
 
 #file { "/etc/dhcp/dhcpd.pools":
 #	ensure	=> present,
@@ -72,22 +76,50 @@ file { "/etc/dhcp/dhcpd.conf":
 #	mode	=> 644,
 #}
 
-file { "/etc/dhcp/dhcpd.hosts":
-	ensure	=> present,
-	source	=> "/vagrant/files/DHCP/dhcpd.hosts",
-	owner	=> root,
-	group	=> root,
-	mode	=> 644,
-}
+#file { "/etc/dhcp/dhcpd.hosts":
+#	ensure	=> present,
+#	source	=> "/vagrant/files/DHCP/dhcpd.hosts",
+#	owner	=> root,
+#	group	=> root,
+#	mode	=> 644,
+#}
 
 # placing the keyfile
-file { "/etc/bind/DHCP_UPDATER":
+file { "/etc/bind/rndc.key":
 	ensure	=> present,
-	source	=> "/vagrant/files/DHCP/DHCP_UPDATER",
+	source	=> "/vagrant/files/DHCP/rndc.key",
 	owner	=> root,
 	group	=> bind,
 	mode	=> 640,
 	require	=> Package["bind9"],
+}
+
+# adding user 'dhcpd' to group 'bind', as this users needs to read the keyfile
+user { "dhcpd":
+	ensure	=> present,
+	groups	=> ['bind'],
+	require => [
+		Package["isc-dhcp-server"],
+		Package["bind9"],
+	],
+}
+
+
+# workaround that DHCP can read the keyfile
+# replace existing DHCPd-apparmor configuration
+service { "apparmor":
+    ensure  => "running",
+    enable  => "true",
+    require => File["/etc/apparmor.d/usr.sbin.dhcpd"],
+}
+
+file { "/etc/apparmor.d/usr.sbin.dhcpd":
+	ensure	=> present,
+	owner	=> root,
+	group	=> root,
+	mode	=> 644,
+	source	=> "/vagrant/files/DHCP/apparmor_usr.sbin.dhcpd",
+	require => Package["isc-dhcp-server"],
 }
 
 # TFTP
@@ -106,6 +138,7 @@ file { '/var/lib/tftpboot/pxelinux.cfg':
 	owner	=> nobody,
 	group	=> nogroup,
 	mode	=> 777,
+	require	=> File["/var/lib/tftpboot"],
 }
 
 # netboot image for Ubunu 12.04
@@ -118,6 +151,7 @@ file { '/var/lib/tftpboot/ubuntu-12.04':
 	group	=> nogroup,
 	mode	=> 777,
 	source	=> "/vagrant/files/TFTP/ubuntu-12.04",
+	require	=> File["/var/lib/tftpboot"],
 }
 
 # config: list of available boot image
@@ -127,6 +161,7 @@ file { '/var/lib/tftpboot/pxelinux.cfg/default':
 	group	=> nogroup,
 	mode	=> 777,
 	source	=> "/vagrant/files/TFTP/default",
+	require	=> File["/var/lib/tftpboot/pxelinux.cfg"],
 }
 
 # boot menu text
@@ -136,6 +171,7 @@ file { '/var/lib/tftpboot/boot.txt':
 	group	=> nogroup,
 	mode	=> 777,
 	source	=> "/vagrant/files/TFTP/boot.txt",
+	require	=> File["/var/lib/tftpboot"],
 }
 
 
@@ -146,13 +182,30 @@ file { "/usr/share/foreman-installer/config/answers.yaml":
 	source	=> "/vagrant/files/answers.yaml",
 	owner	=> root,
 	group	=> root,
-	require	=> [ Package["foreman-installer"], File['/etc/dhcp/dhcpd.conf']]
+	mode	=> 600,
+	require	=> Package["foreman-installer"],
 }
+
+# modifying foreman-installer to support DDNS
+file { "/usr/share/foreman-installer/modules/foreman_proxy/manifests/proxydhcp.pp":
+	ensure	=> present,
+	source	=> "/vagrant/files/proxydhcp.pp",
+	owner	=> root,
+	group	=> root,
+	mode	=> 644,
+	require	=> Package["foreman-installer"],
+}
+
 
 # installation foreman
 exec { 'foreman-installer':
 	command	=> "/usr/bin/foreman-installer",
-	require => [ Package["bind9"], Package["foreman-installer"]],
+	require => [
+		Package["bind9"],
+		File['/usr/share/foreman-installer/modules/foreman_proxy/manifests/proxydhcp.pp'],
+		File['/usr/share/foreman-installer/config/answers.yaml'],
+		File["/etc/bind/rndc.key"]
+	],
 }
 
 
