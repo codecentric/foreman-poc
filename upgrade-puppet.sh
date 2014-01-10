5ab01d9c04cecb01d0701186eb30919503d784e2
Jan-Frederic Markert <Jan-Frederic.Markert@codecentric.de>
2014-01-10 12:35:19 +0100

    Puppet Upgradedok

--- upgrade-puppet.sh
+++ upgrade-puppet.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#wget http://apt.puppetlabs.com/puppetlabs-release-precise.deb
-#sudo dpkg -i puppetlabs-release-precise.deb
+wget https://apt.puppetlabs.com/puppetlabs-release-precise.deb
+sudo dpkg -i puppetlabs-release-precise.deb
 sudo apt-get update
 #sudo apt-get install --yes puppet=3.3.2-1puppetlabs1
 sudo apt-get install --yes puppet
